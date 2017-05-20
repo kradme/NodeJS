d55998d4fcb61cbbca910d556009ca7c75fcee8d
langxp <coolang@yeah.net>
2017-05-21 02:51:49 +0800

    案例

--- heima/static.js
+++ heima/static.js
@@ -4,18 +4,21 @@ var fs = require('fs');
 var path= require('path');
 
 http.createServer(function (req,res) {
+    if(req.url == "/favicon.ico"){
+        return;
+    }
     //获取用户路径
     var pathname = url.parse(req.url).pathname;
     if(pathname == "/"){
-        pathname = 'index.html';
+        pathname += 'index.html';
     }
+    console.log('路径:'+pathname);
     //扩展名
     var extname = path.extname(pathname);
-    console.log(extname);
-    //memi
-    var memi = getMime(extname);
+    var filepath = "./"+path.normalize(pathname);
+    console.log(filepath);
     //读取文件
-    fs.readFile('./static/'+pathname, function (err, data) {
+    fs.readFile(filepath, function (err, data) {
         if(err){
            //如果不存在，返回404
            fs.readFile('./static/404.html',function (err, data) {
@@ -24,16 +27,28 @@ http.createServer(function (req,res) {
            });
            return;
         }
-        res.end(data);
+        //获取mime.json转换为json
+
+        console.log('extname:'+extname);
+        getMime(extname, function (mime) {
+            console.log(mime);
+            res.writeHead(200,{'Content-Type':mime});
+            res.end(data);
+        });
+
 
     });
 }).listen(8000);
 
-function getMime(extname) {
-    switch (extname){
-        case ".html" :
-            return "text/html";
-        case ".jpg" :
-            return "image/jpg";
-    }
+function getMime(extname,callback) {
+    fs.readFile('./static/mime.json', function (err, data) {
+        if(err){
+            throw  err;
+            return;
+        }
+        var mime = JSON.parse(data)[extname];
+        //执行回调函数
+        callback(mime);
+
+    });
 }
